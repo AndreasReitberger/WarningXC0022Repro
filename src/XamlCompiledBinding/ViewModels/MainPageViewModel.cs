a2038897212df59ebb8b2b5f941ac2fb62839286
Andreas <andy.reitberger@gmail.com>
2024-02-16 07:50:15 +0100

    Added source

---
+++ src/XamlCompiledBinding/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamlCompiledBinding.ViewModels
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        #region Properties
+        [ObservableProperty]
+        string name = "This is a name";
+        #endregion
+    }
+}
